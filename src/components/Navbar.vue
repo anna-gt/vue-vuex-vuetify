0e772046f8a7cf861cade4fbb06fc9ac522054b1
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-08-15 12:10:48 +0300

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-container fluid>
+			<v-app-bar
+        color="primary"
+        density="compact"
+      >
+				<v-spacer />
+				<v-btn outlined class="ml-3" color="white">
+					<router-link to="/">Главная</router-link>
+				</v-btn>
+				<v-btn outlined class="ml-3" color="white">
+					<router-link to="/photo">Фото</router-link>
+				</v-btn>
+      </v-app-bar>
+		</v-container>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped>
+
+</style>
